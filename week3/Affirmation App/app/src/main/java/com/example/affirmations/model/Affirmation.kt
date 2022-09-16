ce6a04be94aa19c4c2e70f1a778f4e5f1df70474
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-09-16 18:30:55 +0530

    Primary resources added

---
+++ week3/Affirmation App/app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,8 @@
+package com.example.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(@StringRes val stringRes:Int, @DrawableRes val imageRes:Int){
+
+}
